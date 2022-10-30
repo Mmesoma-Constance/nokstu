a14a785ce866a27ce0c925f688d2a62239c014a4
Mmesoma-Constance <mmesomanwokem@gmail.com>
2022-10-30 01:23:17 +0100

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '128': '36rem',
+      },
+      fontSize: {
+        '4/5xl': '200px',
+      },
+      width: {
+        'wide': '90%',
+        'semi-wide': '23%',
+        'sm-wide': '76%',
+        'smx-wide': '64%',
+      },
+
+    },
   },
   plugins: [],
 }
