ca8274d179c319fab96c5fb870acb93fd831bd81
Mmesoma-Constance <mmesomanwokem@gmail.com>
2022-11-12 23:15:55 +0100

    5th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,21 @@ module.exports = {
     extend: {
       height: {
         '128': '36rem',
+        '110': '27rem',
+        '105': '23rem',
+        '108': '21rem',
         'tall': '40rem',
         '100%': '100%',
         '71': '261.5px',
+        '58': '16rem',
+        '50': '206px',
+        '53': "216",
       },
       fontSize: {
         '4/5xl': '200px',
       },
       width: {
-        'wide': '90%',
+        'wide': '80%',
         'semi-wide': '23%',
         'sm-wide': '76%',
         'smx-wide': '64%',
